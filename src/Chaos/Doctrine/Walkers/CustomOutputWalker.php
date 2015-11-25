5f921321610f0b2f46345ef89f6de21ee69b1d5b
Dzung Nguyen <ntd1712@gmail.com>
2015-11-25 15:24:23 +0700

    Update walker

--- src/Chaos/Doctrine/Walkers/CustomOutputWalker.php
+++ src/Chaos/Doctrine/Walkers/CustomOutputWalker.php
@@ -12,9 +12,12 @@ class CustomOutputWalker extends SqlWalker
     public function walkWhereClause($whereClause)
     {
         $sql = parent::walkWhereClause($whereClause);
+        $parts = explode(' ', $this->getQuery()->getAST()->fromClause->dispatch($this));
 
-        return ($sql ? '(' . $sql . ') AND ' : ' WHERE ') . strtr("application_key = ':app_key'", [
-            ':app_key' => $this->getQuery()->getHint('config')->get('app.key')
+        return ($sql ? $sql . ' AND ' : ' WHERE ') . strtr(":table.:column = ':value'", [
+            ':table' => end($parts),
+            ':column' => 'application_key',
+            ':value' => $this->getQuery()->getHint('config')->get('app.key')
         ]);
     }
 }

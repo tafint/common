97c71dd7718f6216dedf75b4c2fb43d3c8a6875d
Dzung Nguyen <ntd1712@gmail.com>
2015-11-27 11:58:28 +0700

    Update config template

--- config/config.params.php
+++ config/config.params.php
@@ -5,7 +5,6 @@
         'dateFormat' => 'Y-m-d',
         'timeFormat' => 'H:i:s',
         'charset' => 'UTF-8',
-        'defaultPassword' => '******',
         'imageAllowedExt' => 'gif,jpeg,jpg,png',
         'imageMaxSize' => 2097152, // 2MB
         'itemsPerPage' => 10,
@@ -15,10 +14,11 @@
         'copyright' => 'Copyright 2015 by My Company. All Rights Reserved.',
         'title' => 'Admin Panel',
         'theme' => 'homer', // homer, classic
-        'defaultRoute' => 'setting.index',
+        'defaultRoute' => 'setting.index'
     ],
     'paths' => [
         'apiPath' => __DIR__ . '/../public/api',
         'uploadPath' => __DIR__ . '/../public/uploads'
-    ]
+    ],
+    'urls' => []
 ];

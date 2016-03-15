30f282e2321ca5dcc8e93d2ca9c694c08adc7c9c
Dzung Nguyen <ntd1712@gmail.com>
2016-03-15 23:11:47 +0700

    Sample test

--- tests/BaseDoctrineRepositoryTraitTest.php
+++ tests/BaseDoctrineRepositoryTraitTest.php
@@ -3,6 +3,19 @@
 /**
  * Class BaseDoctrineRepositoryTraitTest
  * @author ntd1712
- * @todo
  */
-class BaseDoctrineRepositoryTraitTest extends \PHPUnit_Framework_TestCase {}
+class BaseDoctrineRepositoryTraitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
